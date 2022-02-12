cfe8048bdddd74c1d3e74729abeebb8920290463
ZiomaleQ <r.partyka30@gmail.com>
2022-02-12 21:26:58 +0100

    Add debug console.log

--- mod.ts
+++ mod.ts
@@ -58,6 +58,7 @@ const slashCommands: deploy.ApplicationCommandPartial[] = [
 ];
 
 if (commands.size != slashCommands.length) {
+  console.log("Updating commands")
   deploy.commands.bulkEdit(slashCommands);
 }
 
