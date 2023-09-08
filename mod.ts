1b1923aa96735e1f1336e65563f9646853ab4af8
ZiomaleQ <r.partyka30@gmail.com>
2023-09-08 09:19:59 +0200

    Resolve dynamically path

--- mod.ts
+++ mod.ts
@@ -1,80 +1,16 @@
 import { config } from "./config.ts"
 import {
-  ApplicationCommandPartial, CommandClient, Intents,
-  InteractionResponseFlags, SlashCommandInteraction, SlashCommandOption
+  Client, Intents, InteractionResponseFlags, SlashCommandInteraction
 } from "./deps.ts"
+import { loadSlashCommands } from "./utils/loaders.ts"
 
-const client = new CommandClient({ token: config.TOKEN, prefix: "keiko!" })
-
-type Command = {
-  /** could be a harmony subcommand string too */
-  name: string
-  execute: (cmd: SlashCommandInteraction) => void
-}
-
-async function readDirectory(dir: string, file: Deno.DirEntry) {
-  const contents = [...Deno.readDirSync(`${config.PATH}/${dir}/${file.name}`)]
-  const localCommands: Command[] = []
-
-  const modFile = contents.findIndex(elt => elt.name === 'mod.ts')
-  let localTypedef: ApplicationCommandPartial | undefined = undefined
-
-  if (modFile !== -1) {
-    const { Typedef, Execute } = await import(`${config.PATH}/commands/${file.name}/mod.ts`) as { Typedef: ApplicationCommandPartial, Execute: (cmd: SlashCommandInteraction) => void }
-    localTypedef = Typedef
-
-    if (Execute !== undefined) {
-      localCommands.push({ name: Typedef.name, execute: Execute })
-    }
-
-    contents.splice(modFile, 1)
-  } else {
-    localTypedef = { name: file.name, description: "No description" }
-  }
-
-  for (const subcommand of contents) {
-    if (!subcommand.isFile && subcommand.name.endsWith('.ts') || subcommand.name.endsWith('.js')) continue
-    const { Typedef, Execute } = await import(`${config.PATH}/commands/${file.name}/${subcommand.name}`) as { Typedef: SlashCommandOption, Execute: (cmd: SlashCommandInteraction) => void }
-
-    if (localTypedef.options === undefined) {
-      localTypedef.options = []
-    }
-
-    localTypedef!.options!.push(Typedef)
-    localCommands.push({ name: `${localTypedef.name} ${Typedef.name}`, execute: Execute })
-  }
-
-  return { localCommands, localTypedef }
-}
+const client = new Client({ token: config.TOKEN })
 
 client.on("ready", async () => {
   const commands = await client.interactions.commands.all()
+  const { localCommands, typeDefs } = await loadSlashCommands()
 
-  const localCommands: Command[] = []
-  const typeDefs: ApplicationCommandPartial[] = []
-
-  for (const file of [...Deno.readDirSync(`${config.PATH}/commands`)]) {
-    if (file.isSymlink) continue
-
-    if (file.isFile && file.name.endsWith('.ts') || file.name.endsWith('.js')) {
-      const { Typedef, Execute } = await import(`${config.PATH}/commands/${file.name}`) as { Typedef: ApplicationCommandPartial, Execute: (cmd: SlashCommandInteraction) => void }
-      typeDefs.push(Typedef)
-      localCommands.push({ name: Typedef.name, execute: Execute })
-      continue
-    }
-
-    if (file.isDirectory) {
-
-      const { localTypedef, localCommands: commands } = await readDirectory('commands', file)
-
-      localCommands.push(...commands)
-      typeDefs.push(localTypedef)
-
-      continue
-    }
-  }
-
-  if (commands.size != localCommands.length) {
+  if (commands.size != typeDefs.length) {
     console.log("Updated commands")
     client.interactions.commands.bulkEdit(typeDefs).catch(console.error)
   }
@@ -89,7 +25,6 @@ client.on("ready", async () => {
     },
     since: Date.now(),
   })
-
 
   console.log("Hi, I'm " + client.user?.tag)
 })
@@ -126,6 +61,7 @@ client.on("interactionCreate", async (i) => {
 
   if (i.data.custom_id.startsWith("a/")) {
     const roleID = i.data.custom_id.split("/")[1]!
+
     if (i.member === undefined) {
       return await i.respond({
         flags: InteractionResponseFlags.EPHEMERAL,
@@ -151,8 +87,7 @@ client.on("interactionCreate", async (i) => {
     } catch (_e) {
       await i.respond(
         {
-          content: `Nie mam uprawnienień do tego (edytowanie roli na <#${i.channel!.id
-            }>)`,
+          content: `Nie mam uprawnienień do tego (edytowanie roli na <#${i.channel!.id}>)`,
         },
       )
     }
---
+++ utils/loaders.ts
@@ -0,0 +1,80 @@
+import { SlashCommandInteraction, ApplicationCommandPartial, SlashCommandOption } from "../deps.ts"
+
+export type Command = {
+  /** could be a harmony subcommand string too */
+  name: string
+  execute: (cmd: SlashCommandInteraction) => void
+}
+
+export async function loadSlashCommands() {
+  const localCommands: Command[] = []
+  const typeDefs: ApplicationCommandPartial[] = []
+
+  for (const file of [...Deno.readDirSync(`${getPath()}/commands`)]) {
+    if (file.isSymlink) continue
+
+    if (file.isFile && file.name.endsWith('.ts') || file.name.endsWith('.js')) {
+      const { Typedef, Execute } = await import(`file://${getPath()}/commands/${file.name}`) as { Typedef: ApplicationCommandPartial, Execute: (cmd: SlashCommandInteraction) => void }
+      typeDefs.push(Typedef)
+      localCommands.push({ name: Typedef.name, execute: Execute })
+      continue
+    }
+
+    if (file.isDirectory) {
+
+      const { localTypedef, localCommands: commands } = await readDirectory('commands', file)
+
+      localCommands.push(...commands)
+      typeDefs.push(localTypedef)
+
+      continue
+    }
+  }
+
+  return { localCommands, typeDefs }
+}
+
+async function readDirectory(dir: string, file: Deno.DirEntry) {
+  const contents = [...Deno.readDirSync(`${getPath()}/${dir}/${file.name}`)]
+  const localCommands: Command[] = []
+
+  const modFile = contents.findIndex(elt => elt.name === 'mod.ts')
+  let localTypedef: ApplicationCommandPartial | undefined = undefined
+
+  if (modFile !== -1) {
+    const { Typedef, Execute } = await import(`file://${getPath()}/commands/${file.name}/mod.ts`) as { Typedef: ApplicationCommandPartial, Execute: (cmd: SlashCommandInteraction) => void }
+    localTypedef = Typedef
+
+    if (Execute !== undefined) {
+      localCommands.push({ name: Typedef.name, execute: Execute })
+    }
+
+    contents.splice(modFile, 1)
+  } else {
+    localTypedef = { name: file.name, description: "No description" }
+  }
+
+  for (const subcommand of contents) {
+    if (!subcommand.isFile && subcommand.name.endsWith('.ts') || subcommand.name.endsWith('.js')) continue
+    const { Typedef, Execute } = await import(`file://${getPath()}/commands/${file.name}/${subcommand.name}`) as { Typedef: SlashCommandOption, Execute: (cmd: SlashCommandInteraction) => void }
+
+    if (localTypedef.options === undefined) {
+      localTypedef.options = []
+    }
+
+    localTypedef!.options!.push(Typedef)
+    localCommands.push({ name: `${localTypedef.name} ${Typedef.name}`, execute: Execute })
+  }
+
+  return { localCommands, localTypedef }
+}
+
+function getPath() {
+  const url = decodeURI(new URL(import.meta.url).pathname).split('/')
+
+  url.shift()
+  url.pop()
+  url.pop()
+
+  return url.join('/')
+}
